5593a85670c24e202723079737baf71f615f6e45
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-04 11:30:50 +0530

    put api related variable  to env file form this

--- src/common/api.js
+++ src/common/api.js
@@ -1,8 +1,8 @@
 export const  getCurrentWeatherData = async (city) => {
    
-    const  API_KEY = "9ded7be35af09496a8508071c3028121";
+    
      let response = await fetch(
-       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric  `
+       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
      );
      return response.json();
    };
