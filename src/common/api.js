1a43042145611bc1af166046e0be8773174d4d54
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-04 11:33:50 +0530

    shift all api related variable to env file

--- src/common/api.js
+++ src/common/api.js
@@ -1,6 +1,6 @@
 export const  getCurrentWeatherData = async (city) => {
    
-    
+  
      let response = await fetch(
        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
      );
