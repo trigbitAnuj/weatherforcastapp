64a25ce46de15ab37700da2467c8aae5f9506bb8
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-04 11:24:58 +0530

    add classes to heading tag

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -6,7 +6,7 @@ function WeatherDetails({forcast}) {
   return(
     <>
        {forcast?<section className="flex flex-col justify-center items-center" >
-       <h1 className="city">{name}</h1>
+       <h1 className="city text-2xl">{name}</h1>
               <p className="temp text-3xl">{`${temp?.toFixed(1)}°`}</p>
               <p className="desc">{description}</p>
               <p className="min-max-temp">{`${temp_max?.toFixed(1)}° ${temp_min?.toFixed(1)}°`}</p> 
