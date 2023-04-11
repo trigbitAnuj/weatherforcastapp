5d2f1e00cfd34bac89daff5d3ea3c062c547ff42
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-11 09:50:44 +0530

    refactor from es5 to es6

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,32 +1,38 @@
 import { useContext } from "react";
 import { ThemeContext } from "../App";
 
+const WeatherDetails = ({ data }) => {
+  const theme = useContext(ThemeContext);
 
-
-function WeatherDetails({forcast}) {
-  const theme=useContext(ThemeContext)
-  console.log(theme,"line 6")
-    if(!forcast){
-      return 
-    }
-    const {name,main:{temp,temp_min,temp_max},weather:[{description}]}=forcast??{};
-  return(
-    <>
-       {forcast?<section className="flex flex-col justify-center items-center"  style={{background:theme==="light"?"white":"black", color:theme==="light"?"black":"white"}}    >
-       <h1 className="city text-2xl">{name}</h1>
-              <p className="temp text-3xl">{`${temp?.toFixed(1)}°`}</p>
-              <p className="desc">{description}</p>
-              <p className="min-max-temp ">{`${temp_max?.toFixed(1)}° ${temp_min?.toFixed(1)}°`}</p> 
-         
-     </section>:null} 
-     
-      
-      </>
-     
-    )
+  if (!data) {
+    return null;
   }
-  
-  
-  
-  
-  export default WeatherDetails
+
+  const {
+    name,
+    main: { temp, temp_min, temp_max },
+    weather: [{ description }],
+  } = data ?? { name: "", main: {}, weather: [{}] };
+  return (
+    <>
+      {data ? (
+        <section
+          className="flex flex-col justify-center items-center"
+          style={{
+            background: theme === "light" ? "white" : "black",
+            color: theme === "light" ? "black" : "white",
+          }}
+        >
+          <h1 className="city text-2xl">{name}</h1>
+          <p className="temp text-3xl">{`${temp?.toFixed(1)}°`}</p>
+          <p className="desc">{description}</p>
+          <p className="min-max-temp ">{`${temp_max?.toFixed(
+            1
+          )}° ${temp_min?.toFixed(1)}°`}</p>
+        </section>
+      ) : null}
+    </>
+  );
+};
+
+export default WeatherDetails;
