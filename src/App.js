db5d1aa9e1119f4909a7906eb0eb43e110ec4505
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-11 09:52:24 +0530

    Update App.js from es5 to es6

--- src/App.js
+++ src/App.js
@@ -1,113 +1,117 @@
-import './App.css';
-import React, { useEffect, useState } from 'react'
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-import WeatherDetails from './components/WeatherDetails';
-import { getCurrentWeatherData } from './common/api';
-import { useCallback } from 'react';
-import { createContext } from 'react';
-import { useContext } from 'react';
+import WeatherDetails from "./components/WeatherDetails";
 
+import { useCallback } from "react";
+import { createContext } from "react";
 
+import UseFetch from "./components/UseFetch";
 
+export const ThemeContext = createContext("dark");
 
-export const ThemeContext= createContext("dark");
+const App = () => {
+  const [city, setCity] = useState("Chandigarh");
+  const [changecity, setChangecity] = useState("");
+  const [theme, setTheme] = useState("light");
+  const { data, loading, error } = UseFetch(city);
 
-// export const useTheme=()=>{
-//   useContext(ThemeContext)
-// }
+  const handleClick = useCallback(
+    (changecity) => {
+      setCity(changecity);
+      setChangecity("");
+    },
+    [changecity]
+  );
+  const handleChangeCity = (e) => {
+    setChangecity(e.target.value);
+  };
 
-function App() {
+  //
+  const changeTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
 
-
-  const [forcast,setForcast]=useState(null);
-   const[city, setCity] = useState("Chandigarh");
-   const [changecity,setChangecity]=useState("");
-   const [theme, setTheme] = useState('light')
-   console.log(city);
-   console.log(forcast)
-
- 
-
- const onHandleClick=useCallback((city)=>{
-  console.log("handle Clicked")
-      fetchData(changecity)
-      setCity(city)
-      setChangecity("")
-  },[changecity])
-  
-  
-  
-  async function fetchData(city){
-    const data= await getCurrentWeatherData(city);
-    
-    
-    setForcast(data)
-    console.log(data);
-    
-  }
-  const changeTheme=()=>{
-    setTheme(prev=>prev==="light"?"dark":"light")
-
-console.log(theme)}
-  
-  
-  useEffect(() => {
-    fetchData(city)
-    
-  },[city])
-
-  if(!forcast){
-    return 
-  }
-  
-  
-  
-  
-  
   return (
     <>
-    <ThemeContext.Provider value={theme}>
-    {forcast? <> <div className="flex justify-center my-8 ">
-        <input
-          className="border p-1 border-black"
-          type="text"
-          placeholder="Search City"
-          value={changecity}
-          onChange={(e)=>{setChangecity(e.target.value)
-          }
-}
-        />
-        <button
-          onClick={()=>{onHandleClick(changecity)
-          }}
-          className="mx-2 border border-black p-1"
-        >
-          Search
-        </button>
-      </div>
-      <WeatherDetails forcast={forcast}/>
-      <div className='  flex justify-center my-4 '>
-      <button className='w-1/5 border border-1 border-blue-500 p-2 ' onClick={changeTheme}>Toggle Theme</button>
-      </div>
-      
-      </>:null}
-    </ThemeContext.Provider>
-   
-    
-      
-     
-    
+      <ThemeContext.Provider value={theme}>
+        <>
+          {loading && (
+            <div className=" flex justify-center items-center my-10">
+              <svg
+                class="animate-spin  h-1/5 w-1/5 text-red-700"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </div>
+          )}
+          {error && (
+            <h1 className="text-center text-4xl my-9">
+              {error.message}
+              <div className="flex justify-center my-4">
+                <button
+                  className="border-2 p-1"
+                  onClick={() => {
+                    window.location.reload();
+                  }}
+                >
+                  Back to home
+                </button>
+              </div>
+            </h1>
+          )}
+
+          {data ? (
+            <>
+              <div className="flex justify-center my-8 ">
+                <input
+                  className="border p-1 border-black"
+                  type="text"
+                  placeholder="Search City"
+                  value={changecity}
+                  onChange={(e) => {
+                    handleChangeCity(e);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    handleClick(changecity);
+                  }}
+                  className="mx-2 border border-black p-1"
+                >
+                  Search
+                </button>
+              </div>
+              <WeatherDetails data={data} error={error} />
+              <div className="  flex justify-center my-4 ">
+                <button
+                  className="w-1/5 border border-1 border-blue-500 p-2 "
+                  onClick={changeTheme}
+                >
+                  Toggle Theme
+                </button>
+              </div>
+            </>
+          ) : null}
+        </>
+      </ThemeContext.Provider>
     </>
-
-
-  
-
-
-
-
-
-  )
-}
+  );
+};
 
 export default App;
-
