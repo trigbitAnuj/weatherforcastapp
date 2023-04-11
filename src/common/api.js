c1c80f7e96b5edc7c8bb49052258ca251ed21d08
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-11 18:30:25 +0530

    Update api.js

--- src/common/api.js
+++ src/common/api.js
@@ -1,36 +1,20 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-export const getCurrentWeatherData = async (city) => {
-  try {
-    let response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
-    );
-    return response.json();
-  } catch (error) {
-    return error.message;
-  }
+// export const getCurrentWeatherData = async (city) => {
+//   try {
+//     let response = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
+//     );
+//     return response.json();
+//   } catch (error) {
+//     return error.message;
+//   }
+// };
+
+//
+
+export const getCity = () => {
+  const city = localStorage.getItem("city") ?? "Chandigarh";
+  console.log(city);
+  return city;
 };
-
-//  export const Usefetch=(city)=>{
-//   const
-
-//   useEffect(()=>{
-
-//      const fetchData= async()=>{
-//       try {
-//         const response = await fetch(
-//           `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
-//         );
-//         return response.json();
-
-//       } catch (error) {
-//         return error.message
-//       }
-//     };
-
-//     fetchData()
-
-//      },[city]
-//   )
-//   return { isLoading, apiData, serverError };
-//  }
