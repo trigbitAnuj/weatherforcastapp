287c5811d7ac856911697f25958324de7218d3ea
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-11 09:51:02 +0530

    Update api.js

--- src/common/api.js
+++ src/common/api.js
@@ -1,8 +1,36 @@
-export const  getCurrentWeatherData = async (city) => {
-   
-  
-     let response = await fetch(
-       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
-     );
-     return response.json();
-   };
+import { useEffect } from "react";
+
+export const getCurrentWeatherData = async (city) => {
+  try {
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
+    );
+    return response.json();
+  } catch (error) {
+    return error.message;
+  }
+};
+
+//  export const Usefetch=(city)=>{
+//   const
+
+//   useEffect(()=>{
+
+//      const fetchData= async()=>{
+//       try {
+//         const response = await fetch(
+//           `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric  `
+//         );
+//         return response.json();
+
+//       } catch (error) {
+//         return error.message
+//       }
+//     };
+
+//     fetchData()
+
+//      },[city]
+//   )
+//   return { isLoading, apiData, serverError };
+//  }
